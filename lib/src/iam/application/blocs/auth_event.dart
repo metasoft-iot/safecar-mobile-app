f7240745b5099c199ad9e73410a2e6dc7ada9d46
HOLGUINUPC <u202220250@upc.edu.pe>
2025-11-06 23:40:27 -0500

    feat(iam): add AuthEvent for login, registration, and logout

---
+++ lib/src/iam/application/blocs/auth_event.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+abstract class AuthEvent extends Equatable {
+  const AuthEvent();
+  @override
+  List<Object> get props => [];
+}
+
+class LoginRequested extends AuthEvent {
+  final String email;
+  final String password;
+
+
+  const LoginRequested({required this.email, required this.password});
+  @override
+  List<Object> get props => [email, password];
+}
+
+class RegisterRequested extends AuthEvent {
+  final String email;
+  final String password;
+  final String confirmPassword;
+  final String role;
+
+
+  const RegisterRequested({
+    required this.email,
+    required this.password,
+    required this.confirmPassword,
+    required this.role,
+  });
+  @override
+  List<Object> get props => [email, password, confirmPassword];
+}
+
+class LogoutRequested extends AuthEvent {
+  const LogoutRequested();
+}
