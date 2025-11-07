889444519b393e6e356dac5fd7a11abac05bc5a0
HOLGUINUPC <u202220250@upc.edu.pe>
2025-11-06 23:41:01 -0500

    refactor(router): implement auth-aware routing

---
+++ lib/src/core/router/app_router.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:safecar_mobile_app/src/iam/application/blocs/auth_bloc.dart';
+import 'package:safecar_mobile_app/src/iam/application/blocs/auth_state.dart';
+
+import '../../dashboard/presentation/pages/dashboard_screen.dart';
+import '../../iam/presentation/pages/login_screen.dart';
+import '../../iam/presentation/pages/register_screen.dart';
+
+
+class AppRouter {
+  final AuthBloc authBloc;
+  late final GoRouter router;
+
+  AppRouter(this.authBloc) {
+    router = GoRouter(
+      refreshListenable: _AuthRefreshStream(authBloc.stream),
+
+      initialLocation: '/login',
+
+      routes: [
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          path: '/register',
+          builder: (context, state) => const RegisterScreen(),
+        ),
+        GoRoute(
+          path: '/dashboard',
+          builder: (context, state) => const DashboardScreen(),
+        ),
+        // GoRoute(
+        //   path: '/vehicles',
+        //   builder: (context, state) => const VehiclesScreen(),
+        // ),
+      ],
+
+      redirect: (BuildContext context, GoRouterState state) {
+        final location = state.matchedLocation;
+
+        final authState = authBloc.state;
+
+        final publicRoutes = ['/login', '/register'];
+
+        if (authState is AuthInitial || authState is AuthLoading) {
+          return null;
+        }
+
+        if (authState is Unauthenticated) {
+          if (!publicRoutes.contains(location)) {
+            return '/login';
+          }
+        }
+
+        if (authState is Authenticated) {
+          if (publicRoutes.contains(location)) {
+            return '/dashboard';
+          }
+        }
+
+        return null;
+      },
+    );
+  }
+}
+
+class _AuthRefreshStream extends ChangeNotifier {
+  late final StreamSubscription<AuthState> _subscription;
+  _AuthRefreshStream(Stream<AuthState> stream) {
+    _subscription = stream.asBroadcastStream().listen((_) => notifyListeners());
+  }
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+}
