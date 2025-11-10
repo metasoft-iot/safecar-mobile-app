c0d63257bb07af598b4d3c62d33c6fb16eb7cc9d
JanoverSaldana <u20201b510@upc.edu.pe>
2025-11-09 20:00:48 -0500

    feat(router): configure GoRouter with nested routes

---
+++ lib/core/router/router.dart
@@ -0,0 +1,10 @@
+/// Router exports
+/// Central file to export all router-related classes
+/// Part of Core layer infrastructure
+library router;
+
+export 'app_router.dart';
+export 'route_constants.dart';
+export 'models/bottom_nav_item.dart';
+export 'helpers/navigation_helper.dart';
+export 'helpers/route_params_helper.dart';
