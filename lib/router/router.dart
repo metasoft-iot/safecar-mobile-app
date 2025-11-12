bc1dda15a72583c41ec18bcc91c1b8af9f382b92
JanoverSaldana <u20201b510@upc.edu.pe>
2025-11-12 07:42:58 -0500

    chore: remove unused router helpers

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -2,6 +2,5 @@ library;
 
 export 'app_router.dart';
 export 'route_constants.dart';
-export 'helpers/bottom_nav_item.dart';
 export 'helpers/navigation_helper.dart';
 export 'helpers/route_params_helper.dart';
