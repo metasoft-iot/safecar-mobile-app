fd7c4a227923596fab12be82ecea302ec153cd4e
HOLGUINUPC <u202220250@upc.edu.pe>
2025-11-12 23:42:28 -0500

    feat(iam): extend register method with profile fields

--- lib/src/iam/domain/repositories/auth_repository.dart
+++ lib/src/iam/domain/repositories/auth_repository.dart
@@ -1,19 +1,23 @@
-import '../entities/auth_credentials.dart';
-import '../../domain/entities/user.dart';
-
-
+import 'package:safecar_mobile_app/src/iam/domain/entities/auth_credentials.dart';
+import 'package:safecar_mobile_app/src/iam/domain/entities/user.dart';
 
 abstract class AuthRepository {
 
   Future<AuthCredentials> login(String email, String password);
 
   Future<User> register({
+    // Auth
     required String email,
     required String password,
     required String confirmPassword,
     required List<String> roles,
+    // Profile
+    required String fullName,
+    required String city,
+    required String country,
+    required String phone,
+    required String dni,
   });
 
   Future<void> logout();
-
 }
