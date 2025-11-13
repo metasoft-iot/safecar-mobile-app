bbf656022405f39e2eeac306497a1cb554c1206a
HOLGUINUPC <u202220250@upc.edu.pe>
2025-11-12 23:46:26 -0500

    feat(iam): add ProfileRemoteDataSource

---
+++ lib/src/iam/infrastructure/datasources/profile_remote_datasource.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:safecar_mobile_app/src/iam/infrastructure/models/create_profile_request_model.dart';
+import 'package:safecar_mobile_app/src/iam/infrastructure/models/profile_response_model.dart';
+
+class ProfileRemoteDataSource {
+  final Dio _dio;
+
+  ProfileRemoteDataSource(this._dio);
+
+  /// Crea un nuevo perfil para un usuario existente
+  Future<ProfileResponseModel> createProfile({
+    required String userEmail, // Ahora pide el email
+    required CreateProfileRequestModel profileData,
+  }) async {
+    try {
+      final response = await _dio.post(
+        '/api/v1/person-profiles', // Nuevo endpoint
+        queryParameters: {
+          'userEmail': userEmail, // Nuevo query parameter
+        },
+        data: profileData.toJson(),
+      );
+
+      return ProfileResponseModel.fromJson(response.data);
+
+    } on DioException catch (e) {
+      print("Error en createProfile: $e");
+      String errorMessage = 'Error al crear el perfil.';
+      if (e.response?.data != null) {
+        errorMessage = e.response?.data['message'] ?? errorMessage;
+      }
+      throw Exception(errorMessage);
+    }
+  }
+}
